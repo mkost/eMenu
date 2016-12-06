87533e54ec1c1110efd19cbde6294527f86d0659
Maciej Kostuch <kostuchmaciek@gmail.com>
2016-12-06 20:59:53 +0100

    change conception a litte - one dish to one card

--- emenu/admin.py
+++ emenu/admin.py
@@ -4,11 +4,11 @@ from .models import Dish
 from .models import Card
 
 
-@admin.register(Dish)
-class DishAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'price', 'preparation_time', 'is_vegetarian')
+class DishInline(admin.StackedInline):
+    model = Dish
 
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')
+    inlines = [DishInline,]
--- emenu/models.py
+++ emenu/models.py
@@ -3,37 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
-class Dish(models.Model):
-    name = models.CharField(u'nazwa', max_length=255)
-    description = models.TextField(u'opis')
-    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    preparation_time = models.IntegerField(help_text=u'w minutach')
-    created = models.DateTimeField(editable=False)
-    updated = models.DateTimeField(editable=False)
-    is_vegetarian = models.BooleanField(default=False)
-    picture = models.ImageField(u'zdjęcie', null=True, blank=True, upload_to='dishes/')
-
-    class Meta:
-        verbose_name = u'danie'
-        verbose_name_plural = u'dania'
-
-    def __unicode__(self):
-        return "%s" % (self.name,)
-
-    def save(self, *args, **kwargs):
-        """ On save, update timestamps """
-        if not self.id:
-            self.created = timezone.now()
-        self.updated = timezone.now()
-        return super(Dish, self).save(*args, **kwargs)
-
-
 class Card(models.Model):
     name = models.CharField(u'nazwa', max_length=255, unique=True)
     description = models.TextField(u'opis')
     created = models.DateTimeField(editable=False)
     updated = models.DateTimeField(editable=False)
-    dishes = models.ManyToManyField(Dish, blank=True)
 
     class Meta:
         ordering = ['id']
@@ -49,3 +23,29 @@ class Card(models.Model):
             self.created = timezone.now()
         self.updated = timezone.now()
         return super(Card, self).save(*args, **kwargs)
+
+
+class Dish(models.Model):
+    name = models.CharField(u'nazwa', max_length=255)
+    description = models.TextField(u'opis')
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    preparation_time = models.IntegerField(help_text=u'w minutach')
+    created = models.DateTimeField(editable=False)
+    updated = models.DateTimeField(editable=False)
+    is_vegetarian = models.BooleanField(default=False)
+    card = models.ForeignKey(Card, null=True, blank=True)
+    picture = models.ImageField(u'zdjęcie', null=True, blank=True, upload_to='dishes/')
+
+    class Meta:
+        verbose_name = u'danie'
+        verbose_name_plural = u'dania'
+
+    def __unicode__(self):
+        return "%s" % (self.name,)
+
+    def save(self, *args, **kwargs):
+        """ On save, update timestamps """
+        if not self.id:
+            self.created = timezone.now()
+        self.updated = timezone.now()
+        return super(Dish, self).save(*args, **kwargs)
